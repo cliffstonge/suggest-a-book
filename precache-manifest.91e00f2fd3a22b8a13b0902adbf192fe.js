17984bc370f63e9c8985d4cfef8170642eddf692
cliffstonge <cliffstonge@gmail.com>
2020-06-22 22:34:42 -0400

    Updates

---
+++ precache-manifest.91e00f2fd3a22b8a13b0902adbf192fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3980c6f8f721e6d2a987d2c18c103092",
+    "url": "/suggest-a-book/index.html"
+  },
+  {
+    "revision": "e7ca95f41541a3ef35d2",
+    "url": "/suggest-a-book/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e29f99f66ab67be0bb9",
+    "url": "/suggest-a-book/static/js/2.0888ab2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/suggest-a-book/static/js/2.0888ab2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ca95f41541a3ef35d2",
+    "url": "/suggest-a-book/static/js/main.eabb8b40.chunk.js"
+  },
+  {
+    "revision": "1289d2ba8af3bf774b72",
+    "url": "/suggest-a-book/static/js/runtime-main.bea2329c.js"
+  }
+]);
