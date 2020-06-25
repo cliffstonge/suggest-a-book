b9ff8bbe49e9852d7ac0fad26a43d6fa55bc0cb5
cliffstonge <cliffstonge@gmail.com>
2020-06-24 23:57:39 -0400

    Changed fieldset to grid

--- src/Form.js
+++ src/Form.js
@@ -7,30 +7,32 @@ class Form extends Component {
                 <form>
                     <fieldset>
                         <legend>Choose a genre:</legend>
-
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:fantasy"/>Fantasy</label>
-
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:science-fiction"/>Sci-Fi</label>
-
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:mystery"/>Mystery</label>
                         
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:thriller"/>Thriller</label>
-                       
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:poetry"/>Poetry</label>
-                        
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:romance"/>Romance</label>
-                       
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:history"/>History</label>
-                        
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:art"/>Art</label>
-                       
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:biography"/>Biography</label>
-                       
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:sports"/>Sports</label>
-                        
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:travel"/>Travel</label>
-
-                        <label><input onChange={this.props.handleChange} type="radio" name="genre" value="isbn:0451184106"/>Stargate</label>
+                        <div className="gridParent">
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:fantasy"/>Fantasy</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:science-fiction"/>Sci-Fi</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:mystery"/>Mystery</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:thriller"/>Thriller</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:poetry"/>Poetry</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:romance"/>Romance</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:history"/>History</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:art"/>Art</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:biography"/>Biography</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:sports"/>Sports</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="subject:travel"/>Travel</label>
+                            
+                            <label><input onChange={this.props.handleChange} type="radio" name="genre" value="isbn:0451184106"/>Stargate</label>
+                        </div>
                     </fieldset>
 
                     <button onClick={this.props.handleClick}>Find me a book!</button>
